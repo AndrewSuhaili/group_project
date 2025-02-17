e1a4b6fafc9f4b96ab9afcfa29854baebc68b770
Andrew <andrew.suhaili@gmail.com>
2025-02-18 09:13:16 +1100

    t

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Module1 from './components/Module1';
 import Module2 from './components/Module2';
+import Module3 from './components/Module3';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -14,7 +15,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/module1" element={<Module1 />} />
         <Route path="/module2" element={<Module2 />} />
-        <Route path="/module3" element={<div style={{ padding: '2rem' }}>Module 3 coming soon!</div>} />
+        <Route path="/module3" element={<Module3 />} />
         <Route path="/module4" element={<div style={{ padding: '2rem' }}>Module 4 coming soon!</div>} />
       </Routes>
       <Footer />
